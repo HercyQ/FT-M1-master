21d0ccfcdbee22a03f5548fc947accd88f301432
unknown <hercydeveloperfullstack2022@gmail.com>
2022-08-03 00:50:36 -0500

    first commit

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -7,9 +7,21 @@ function BinarioADecimal(num) {
 
 function DecimalABinario(num) {
   // tu codigo aca
+  let resultado = 0;
+  let res = 0;
+  let a_res = [];
 
+  do {
+    resultado = Math.floor(num / 2);
+    res = num - (resultado * 2);
+    a_res.push(res);
+    num = resultado;
+  } while (resultado !== 0);
+  
+  a_res.reverse();
+
+  return a_res.join('');
 }
-
 
 module.exports = {
   BinarioADecimal,
