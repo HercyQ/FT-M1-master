7c9c4ee520b5621b0ebf8a940ec8f71daf5892d6
HercyQ <hercydeveloperfullstack2022@gmail.com>
2022-08-12 23:21:41 -0500

    first commit

--- 04-EstructuraDeDatos-I/homework/homework.js
+++ 04-EstructuraDeDatos-I/homework/homework.js
@@ -1,4 +1,4 @@
-'use strict'
+"use strict";
 
 /*
 Definir las funciones recursivas nFactorial y nFibonacci.
@@ -15,9 +15,21 @@ Como ejercicio adicional y completamente opcional, al terminar de resolver este 
 */
 
 function nFactorial(n) {
+  if (n > -1 && n < 2) {
+    return 1;
+  } else if (n < 0) {
+    return 0;
+  }
+  return n * nFactorial(n - 1);
 }
 
+nFactorial(5);
+nFactorial(15);
+
 function nFibonacci(n) {
+  if (n <= 0) return 0;
+  else if (n === 1) return 1;
+  return nFibonacci(n - 1) + nFibonacci(n - 2);
 }
 
 /*
@@ -39,5 +51,5 @@ function Queue() {
 module.exports = {
   Queue,
   nFactorial,
-  nFibonacci
+  nFibonacci,
 };
