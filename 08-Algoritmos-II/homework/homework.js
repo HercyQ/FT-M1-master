523d708d21a58adecd583ea69e57c5240e697d64
HercyQ <hercydeveloperfullstack2022@gmail.com>
2022-08-29 01:14:34 -0500

    First commit

--- 08-Algoritmos-II/homework/homework.js
+++ 08-Algoritmos-II/homework/homework.js
@@ -1,4 +1,4 @@
-'use strict'
+"use strict";
 // No cambies los nombres de las funciones.
 
 function quickSort(array) {
@@ -6,7 +6,19 @@ function quickSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
+  var pivot = array[0];
+  var left = [];
+  var right = [];
 
+  if (array.length <= 1) {
+    return array;
+  }
+
+  for (var i = 1; i < array.length; i++) {
+    array[i] < pivot ? left.push(array[i]) : right.push(array[i]);
+  }
+
+  return quickSort(left).concat(pivot, quickSort(right));
 }
 
 function mergeSort(array) {
@@ -15,6 +27,32 @@ function mergeSort(array) {
   // Devolver el array ordenado resultante
   // Tu código:
 
+  //_mergeArrays une ambos arrays
+  const _mergeArrays = (left, right) => {
+    const c = [];
+
+    while (left.length && right.length) {
+      c.push(left[0] > right[0] ? right.shift() : left.shift());
+    }
+
+    //si todavía tenemos valores, agréguelos al final de `c`
+    while (left.length) {
+      c.push(left.shift());
+    }
+    while (right.length) {
+      c.push(right.shift());
+    }
+    return c;
+  };
+
+  if (array.length <= 1) return array;
+
+  let mid = parseInt(array.length / 2);
+
+  let left = array.slice(0, mid);
+  let right = array.slice(mid, array.length);
+
+  return _mergeArrays(mergeSort(left), mergeSort(right));
 }
 
 // No modificar nada debajo de esta línea
